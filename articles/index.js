1df7158153711517c04dd85089909d608b4d479b
Ian Cooper <me@iancooper.name>
2012-12-30 17:51:46 -0700

    updated

--- articles/index.js
+++ articles/index.js
@@ -1,2 +1,22 @@
-articles = {
-};
+articles = [
+    {
+	"date": "2012-12-06T1430",
+	"title": "Test #1",
+	"slug": "test-01",
+	"file": "2012-12-06-0230-test-01.md"
+    },
+
+    {
+	"date": "2012-12-06T1433",
+	"title": "Test #2",
+	"slug": "test-02",
+	"file": "2012-12-06-0233-test-01.md"
+    },
+
+    {
+	"date": "2012-12-06T1445",
+	"title": "Test #3",
+	"slug": "test-03",
+	"file": "2012-12-06-0245-test-01.md"
+    }
+];
