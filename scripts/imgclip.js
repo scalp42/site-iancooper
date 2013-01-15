34dfb149cb81960dd202b684f7a663f73dc06154
Ian Cooper <me@iancooper.name>
2013-01-15 13:34:26 -0700

    added imgclip javascript

---
+++ scripts/imgclip.js
@@ -0,0 +1,38 @@
+$(function() {
+
+    var pasteHandler = function(event) {
+	// check for clipboard data
+	if (event.clipboardData) {
+	    
+	    // get the clipboard contents
+	    if (var items = event.clipboardData.items) {
+		
+		console.dir(items);
+
+		// loop through the clipboard items
+		for (var i = 0; i < items.length; i++) {
+		    
+		    // check for image data
+		    if (items[i].type.indexOf("image") !== -1) {
+
+			// get the image data
+			var blob = items[i].getAsFile();
+
+			// create a URL
+			var source = window.URL.createObjectURL(blob);
+
+			// show the image
+			var image = document.createElement('img');
+			img.src = source;
+			document.getElementById('clipboard').appendChild(image);
+		}
+
+	    }
+	}
+    }
+
+    // check for clipboard support
+    if (window.Clipboard) {
+	window.addEventListener("paste", pasteHandler);
+    }
+});
