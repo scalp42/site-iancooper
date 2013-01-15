419f9638382b79b0b0fc9169e22919aecc792571
Ian Cooper <me@iancooper.name>
2013-01-14 17:20:31 -0700

    updated

--- scripts/139airports.js
+++ scripts/139airports.js
@@ -38,10 +38,10 @@ $(function() {
 	    
 	    // put together some text to go in the info window
 	    info = '<div>Airport ID: <strong>' +
-		   event.row.LocationID.value + '</strong><hr />' +
+		   event.row.LocationID.value + '</strong><br />' +
 		   event.row.FacilityName.value + '<br />' +
 		   event.row.City.value + ', ' +
-		   event.row.State.value + '<hr />'
+		   event.row.State.value + '<br />'
 	           'Elevation: ' + event.row.Elevation.value + ' MSL<br />' + 
 		   'Sectional Chart: ' + event.row.ChartName.value + '</div>'
 
